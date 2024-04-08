10f181fdc73bf80d62023fbc4e64e4e8fad2cf90
Michael OBrien <michaelob@outlook.com.au>
2024-04-08 12:02:00 +1000

    added 1 more cypress test

--- cypress/e2e/pokemonPage.cy.js
+++ cypress/e2e/pokemonPage.cy.js
@@ -1,13 +1,15 @@
 describe('Pokedex', function () {
   it('front page can be opened', () => {
     cy.visit('http://localhost:5001');
-    cy.contains('ivysaur').click();
-    cy.contains('overgrow');
+    cy.contains('ivysaur');
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     );
   });
-});
 
-// upto Three options are used:
-// actions failing in github, find errors and fix them and get cypress working on CI/CD
+  it('pokemon page can be navigated to', () => {
+    cy.visit('http://localhost:5001');
+    cy.contains('charmander').click();
+    cy.contains('blaze');
+  });
+});
